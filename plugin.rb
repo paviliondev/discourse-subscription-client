78583e3512f649d6078067cad3b99d12d54f18c8
Angus McLeod <angus@mcleod.org.au>
2023-03-14 16:40:58 +0100

    Bump version

--- plugin.rb
+++ plugin.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 # name: discourse-subscription-client
 # about: Subscription client
-# version: 0.1.2
+# version: 0.2.0
 # authors: Robert Barrow, Angus McLeod
 # url: https://github.com/paviliondev/discourse-subscription-client.git
 # contact_emails: development@pavilion.tech
