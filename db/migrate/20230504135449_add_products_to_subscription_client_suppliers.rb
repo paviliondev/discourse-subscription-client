f5ef25423cf00d03e9396b8df5a6da9ce207941b
Angus McLeod <angus@mcleod.org.au>
2023-05-09 09:14:40 +0200

    Add existence check to products column migration

--- db/migrate/20230504135449_add_products_to_subscription_client_suppliers.rb
+++ db/migrate/20230504135449_add_products_to_subscription_client_suppliers.rb
@@ -2,6 +2,6 @@
 
 class AddProductsToSubscriptionClientSuppliers < ActiveRecord::Migration[7.0]
   def change
-    add_column :subscription_client_suppliers, :products, :json
+    add_column :subscription_client_suppliers, :products, :json, if_not_exists: true
   end
 end
