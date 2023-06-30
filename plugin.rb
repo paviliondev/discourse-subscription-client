c00d5bb3c6e7e0fbe88d86e69092cdfa77f5fb1e
Robert Barrow <merefield@gmail.com>
2023-06-30 17:04:53 +0100

    bump patch

--- plugin.rb
+++ plugin.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 # name: discourse-subscription-client
 # about: Subscription client
-# version: 0.2.2
+# version: 0.2.3
 # authors: Robert Barrow, Angus McLeod
 # url: https://github.com/paviliondev/discourse-subscription-client.git
 # contact_emails: development@pavilion.tech
