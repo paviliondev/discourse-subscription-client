d7e6dded559655e56826692dc932eddbfe79bbc8
merefield <merefield@gmail.com>
2023-09-22 12:25:54 +0100

    help with development of standalone plugin subs system

--- plugin.rb
+++ plugin.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 # name: discourse-subscription-client
 # about: Subscription Client for Plugin Subscriptions
-# version: 0.3.0
+# version: 0.3.1
 # authors: Robert Barrow, Angus McLeod
 # url: https://github.com/paviliondev/discourse-subscription-client.git
 # contact_emails: development@pavilion.tech
@@ -10,4 +10,4 @@ register_asset 'stylesheets/common/common.scss'
 register_svg_icon "far-building"
 add_admin_route "admin.subscription_client.title", "subscriptionClient"
 
-gem "discourse_subscription_client", "0.1.0.pre14", require_name: "discourse_subscription_client"
+gem "discourse_subscription_client", "0.1.0.pre15", require_name: "discourse_subscription_client"
