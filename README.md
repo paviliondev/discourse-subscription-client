20205d810c9b71f6f70f005e0d8247695a99687f
Angus McLeod <angus@mcleod.org.au>
2023-12-08 10:45:00 +0100

    Update message and message placement

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # Discourse Subscription Client
 
-This has been deprecated, please do not use.
+Pavilion is happy to announce that Custom Wizard Subscriptions no longer require this plugin to function. If you have any questions don't hestiate to reach out to us on [coop.pavilion.tech]("https://coop.pavilion.tech") or email support@pavilion.tech.
--- plugin.rb
+++ plugin.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 # name: discourse-subscription-client
-# about: Important!: The Subscription Client has now been deprecated, please remove from app.yml before next rebuild.
+# about: Custom Wizard Subscriptions no longer require this plugin. Please remove it before your next rebuild.
 # version: 0.4.0
 # authors: Robert Barrow, Angus McLeod
 # url: https://github.com/paviliondev/discourse-subscription-client.git
