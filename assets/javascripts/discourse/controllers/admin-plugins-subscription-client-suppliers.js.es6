93303d9b70c5574413978b2eea8793232fc201c3
merefield <merefield@gmail.com>
2023-12-08 08:37:49 +0000

    Add deprecation messages, update README

--- assets/javascripts/discourse/controllers/admin-plugins-subscription-client-suppliers.js.es6
+++
@@ -1,9 +0,0 @@
-import Controller from "@ember/controller";
-import { notEmpty } from "@ember/object/computed";
-
-export default Controller.extend({
-  classNameBindings: [":suppliers"],
-  hasSuppliers: notEmpty("suppliers"),
-  messageKey: "info",
-  messageClass: "info",
-});
--- plugin.rb
+++ plugin.rb
@@ -1,13 +1,9 @@
 # frozen_string_literal: true
 # name: discourse-subscription-client
-# about: Subscription Client for Plugin Subscriptions
-# version: 0.3.0
+# about: Important!: The Subscription Client has now been deprecated, please remove from app.yml before next rebuild.
+# version: 0.4.0
 # authors: Robert Barrow, Angus McLeod
 # url: https://github.com/paviliondev/discourse-subscription-client.git
 # contact_emails: development@pavilion.tech
 
 register_asset 'stylesheets/common/common.scss'
-register_svg_icon "far-building"
-add_admin_route "admin.subscription_client.title", "subscriptionClient"
-
-gem "discourse_subscription_client", "0.1.0.pre15", require_name: "discourse_subscription_client"
